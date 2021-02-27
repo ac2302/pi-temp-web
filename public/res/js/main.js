6d1c9e30e17fb40dcf8de95aea313399e5f8c1f7
Aryan Chavan <aryanchavan2003@gmail.com>
2021-02-27 06:44:24 +0000

    add changing temperature color

--- public/res/js/main.js
+++ public/res/js/main.js
@@ -2,16 +2,21 @@ const socket = io();
 
 let updateTime = 1000;
 
-
 // update updateTime
 document.getElementById("update-time").addEventListener("change", (e) => {
 	updateTime = e.target.value;
 	document.getElementById("update-time-display").innerText = updateTime;
 });
 
-
 function setTemp(temp) {
-	document.getElementById("temp").innerText = temp;
+	document.getElementById("temp").innerText = temp + "°C";
+	let color = "#FFF";
+	if (temp <= 50) {
+		color = "#0F0";
+	} else if (temp > 80) {
+		color = "#F00";
+	}
+	document.getElementById("temp").style.color = color;
 }
 
 socket.on("temp", (temp) => {
@@ -32,5 +37,3 @@ async function sendUpdateRequest() {
 }
 
 sendUpdateRequest();
-
-
